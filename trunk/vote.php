dc9194b88fbe4a3483e9bded9f048f94a4cd0d75
doggie52 <doggie52@0bc76b2c-0749-11df-a26f-8b716316a5bc>
2010-01-22 19:23:49 +0000

    Various style-changes to code. Moved database-configruation to a separate file.

--- trunk/vote.php
+++ trunk/vote.php
@@ -1,41 +1,21 @@
 <?php
-/*******************************************************************************
-	Filename				: vote.php
-
-	Created					: 04 August 2008 (19:56:56)
-	Created by				: Douglas Stridsberg
-
-	Last Updated			: 08 August 2008 (14:13:07)
-	Updated by				: Douglas Stridsberg
-
-	QueryString Collection	: 
-	Form Collection			: 
-	Session Variables		: 
-	Database Tables			: 
-	Stored Procedures		: 
-	Include Files			: includes/class.php
-
-	Comments				: 
-*******************************************************************************/	
-?>
-<?php
 
 // Includes
 include("includes/class.php");
 
-  // Creates the DB connection
-  $DB = new DB();
-      $DB->connect();
+	// Creates the DB connection
+	$DB = new DB();
+	$DB->connect();
       
-  // Creates the default poll
-  $poll = new poll();
+		// Creates the default poll
+		$poll = new poll();
   
-      // Process the vote
-      $poll->votePoll($_GET['vote'], $_GET['id']);
+			// Process the vote
+			$poll->votePoll($_GET['vote'], $_GET['id']);
       
-          // If the vote was valid, show results
-          if ($poll->handled==1){
-              $poll->displayPoll();
-          }
+				// If the vote was valid, show results
+				if ($poll->handled==1){
+					$poll->displayPoll();
+				}
   
 ?>
