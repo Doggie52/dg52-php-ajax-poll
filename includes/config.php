742ffbc03bd0a3fb225668c2c6ff6457016c031a
doggie52 <doggie52@0bc76b2c-0749-11df-a26f-8b716316a5bc>
2010-01-22 19:23:49 +0000

    Various style-changes to code. Moved database-configruation to a separate file.

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+
+	// Declare array
+	$config = array();
+
+	// Set values
+	$config['dbHost']	= "localhost";
+	$config['dbPort']	= '3306';
+	$config['dbName']	= "voting";
+	$config['dbUser']	= "root";
+	$config['dbPassw']	= "Passw0rd";
+
+?>
