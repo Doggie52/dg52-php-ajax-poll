0de6ee7d98f0f5cc6571fce23d78dbd545684be0
doggie52 <doggie52@0bc76b2c-0749-11df-a26f-8b716316a5bc>
2010-01-30 20:48:18 +0000

    Template class populated. Not tested or implemented in any way yet, heaving testing needs to be done.

--- trunk/includes/class_template.php
+++ trunk/includes/class_template.php
@@ -9,20 +9,48 @@ class template {
 			<code>templates</code> directory does not exist!";
 			exit;
 		}
+		require "includes/template_dictionary.php";
 	}
-	
+
 	private function checkExist($templateName){
 	// Checks a $templateName's existence - exit if non-existant
+		if(!file_exists("templates/".$templateName.".tpl")){
+			echo "Fatal error!
+			<code>$templateName.tpl</code> does not exist!";
+			exit;
+		}
 	}
-	
+
 	private function loadTemplate($templateName){
 	// Loads $templateName
+		if($loadedTemplate = file_get_contents("templates/".$templateName.".tpl", FILE_USE_INCLUDE_PATH)){
+			return $loadedTemplate;
+		}else{
+			echo "Fatal error!
+			<code>$templateName.tmpl</code> can not be loaded for reading!";
+			exit;
+		}
 	}
-	
+
 	private function replaceTemplateVars($templateName, $dictionary){
 	// Replaces variables in $templateName with those found in $dictionary
+	// Tutorial used: http://www.php.net/manual/en/function.str-replace.php#95198
+
+		// Declare arrays
+		$varArray			= array();
+		$translatedArray	= array();
+
+		// For each dictionary entry, split the variable and the translation into two arrays
+		foreach($dictionary as $b => $a){
+			$varArray[] = "%".$b."%";
+			$translatedArray[] = $a;
+		}
+
+		// Replaces the variables with their translated values
+		$translatedTemplate = str_replace($varArray, $translatedArray, $templateName);
+		return $translatedTemplate;
 	}
-	
+
 	private function outputTemplate($templateName){
 	// Outputs the given $templateName with the exchanged variables
 	}
